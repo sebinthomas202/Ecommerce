db10d5f7d8cae9b096df5726643d46193a56619e
Sebin Thomas <167002779+sebinthomas202@users.noreply.github.com>
2024-12-07 23:28:58 +0530

    Update AlertDialogSlide.jsx

--- Frontend/src/components/AlertDialogSlide.jsx
+++ Frontend/src/components/AlertDialogSlide.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
 import { GreenButton, RedButton } from "../utils/buttonStyles";
 
+
 const AlertDialogSlide = ({ dialog, showDialog, setShowDialog, taskHandler }) => {
     const handleClose = () => {
         setShowDialog(false);
